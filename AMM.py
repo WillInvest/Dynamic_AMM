93db7446380a26e3d7de24c82b3c142db4052fe0
Zhiyuan Yao <yaozhiyuan941225@hotmail.com>
2023-11-08 12:23:26 -0500

    Clean

--- AMM.py
+++ AMM.py
@@ -71,14 +71,6 @@ class AMM(ABC):
         # Number of assets excluding L, ie A, B -> 2
         self.num_assets = len(self.portfolio) - 1
 
-        # This loops through each asset in the portfolio
-        # Sets L = to the Nth root of the product
-        # p = 1.
-        # for key in self.portfolio:
-        #     if key != 'L':
-        #         p *= self.portfolio[key]
-        # self.portfolio['L'] = p ** (1 / self.num_assets)
-        
         num_l = self.utility_func.cal_liquid_token_amount(self.portfolio)
         self.portfolio['L'] = num_l
         
@@ -106,46 +98,6 @@ class AMM(ABC):
             ret += f"F{fee}: {self.fees[fee]}\n"
         ret += '-' * 20 + '\n'
         return ret
-
-    # @staticmethod
-    # # Checks that each value in the portfolio aside from L is not 0
-    # # Finds value based on log: Can Zhiyuan explain this?
-    # def _utility_log(portfolio):
-    #     p = 0.
-    #     for key in portfolio:
-    #         if key != 'L':
-    #             assert portfolio[key] > 0, f"invalid value: {key}: {portfolio[key]}"
-    #             p += np.log(portfolio[key])
-    #     return p
-
-    # @staticmethod
-    # def _v_log(l, n):
-    #     # Check both l and n are greater than 0
-    #     assert l > 0 and n > 0, f'invalid value: l = {l}, n = {n}'
-    #     # Return for what??
-    #     return n * np.log(l)
-
-    # @staticmethod
-    # def _utility(portfolio: dict):
-    #     # Return the value of all assets multiplied together
-    #     # Not including L
-    #     p = 1.
-    #     for key in portfolio:
-    #         if key != 'L':
-    #             assert portfolio[key] > 0, f"invalid value: {key} - {portfolio[key]}"
-    #             p *= portfolio[key]
-    #     return p
-
-    # @staticmethod
-    # def _v(l, n):
-    #     # Check both l and n are greater than 0
-    #     assert l > 0 and n > 0, f'invalid value: l = {l}, n = {n}'
-    #     return l ** n
-
-    # def utility(self) -> float:
-    #     # Calculate portfolio using log for easier calc
-    #     # Convert to normal for readability
-    #     return np.exp(self._utility_log(self.portfolio))
 
     def track_asset_ratio(self, asset_to_track, reference_asset):
         self.AfB.append(
@@ -236,29 +188,6 @@ class AMM(ABC):
         fee_dict = self.fee_structure.calculate_fee({s1: s1_in, s2: s2_in}, s2)
         info = {'asset_delta': {s1: s1_in, s2: s2_in}, 'fee': fee_dict}
         return s1_in, info
-
-    
-
-        
-    # def detect_arbitrage(self, market_price):
-    # # Assuming amm has asset pairs and tracks asset ratios
-    # # Calculate implied prices for different asset pairs
-    #     for i in range(len(self.AfB)):
-    #         # Calculate implied price for A in terms of B
-    #         implied_price_A_in_B = self.AfB[i] * market_price
-    #         # Calculate implied price for B in terms of A
-    #         implied_price_B_in_A = self.BfA[i] * (1 / market_price)
-
-    #         # Check if there's an arbitrage opportunity
-    #         if implied_price_A_in_B > round(1 / implied_price_B_in_A,2):
-    #             print(f"Arbitrage Opportunity Detected!")
-    #             print(f"Implied Price of A in terms of B: {implied_price_A_in_B}")
-    #             print(f"Implied Price of B in terms of A: {implied_price_B_in_A}")
-    #             print(f"Execute trades to take advantage of this opportunity")
-    #             # You can place the necessary trade logic here
-    #         else:
-    #             print("No Arbitrage Opportunity Detected.")
-
 
     
 
